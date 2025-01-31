6b3c02bd942cafe5f65132599cd4890ab821fcaa
Estifanos Girma <estifanosgirma4@gmail.com>
2025-01-31 20:57:23 +0300

    qoute

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const newQuotButton = document.querySelector('#newQuote');
     const quoteDisplay = document.querySelector('#quoteDisplay');
-    const child = document.getElementsByClassName('child');
+   
     const quotes = [
         { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
         { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", category: "Inspiration" },
         { text: "Life is 10% what happens to us and 90% how we react to it.", category: "Life" }
       ];
-    newQuotButton.addEventListener('click', function displayRandomQuotes(){
+   
        
-      
+    function showRandomQuote() {   
         const random = Math.floor(Math.random()*quotes.length);
         let quote = quotes[random];
         const p = document.createElement('p');
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         // setTimeout(()=>{
         //     p.style.display = "none"
         // },500)
-    });
+    }
     let addQuoteButton = document.querySelector('#addQuoteButton')
     function addQuote() {
         const newQuoteText = document.getElementById('newQuoteText').value;
@@ -37,6 +37,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
           alert('Please fill in both fields to add a new quote.');
         }
       };
-      addQuoteButton.addEventListener('click', addQuote);
-   
+    addQuoteButton.addEventListener('click', addQuote);
+    newQuotButton.addEventListener('click', showRandomQuote);
 });
